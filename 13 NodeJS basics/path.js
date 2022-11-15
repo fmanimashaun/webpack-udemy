b48e5a19b64e031a8a68a889c09f6e5eb96020ba
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-15 22:52:01 +0100

    Reset solution folder

--- 13 NodeJS basics/path.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-// console.log(path.dirname('../gitignore'));
-// console.log(path.parse('../gitignore'));
-// console.log(path.resolve('../gitignore'));
-console.log(path.join("..", "gitignore"));
--- 13 NodeJS basics/regex.js
+++
@@ -1,9 +0,0 @@
-const re = /go+d/
-
-const greeting = "god morning"
-const str = "abc.jpeg";
-const str2 = "abc.png";
-const str3 = "abc.gif";
-
-console.log(greeting.match(re));
-
