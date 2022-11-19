c4adeb7763733b93e89156e9127a57441760a297
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-20 00:07:00 +0100

    Updated

---
+++ 1. Getting Started/src/js/index.js
@@ -0,0 +1,3 @@
+import style from '../css/styles.scss';
+
+import { addImage } from '../img/AddForm.png';
--- 1. Getting Started/webpack.config.js
+++ 1. Getting Started/webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,14 +6,33 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'img/[hash][ext][query]',
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+  }
+  )],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      }
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader', options: { modules: true } }],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader', options: { modules: true } }, { loader: 'sass-loader' }],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ]
   }
 };
